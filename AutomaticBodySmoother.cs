a42719f12a8a0e73a8fda63666f0fb7e6deb971e
Acid Bubbles <acidbubbles.anon@gmail.com>
2021-07-26 12:17:14 -0400

    Further simplify syntax

--- AutomaticBodySmoother.cs
+++ AutomaticBodySmoother.cs
@@ -1,35 +1,28 @@
 using System;
 using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using MeshVR;
-using SimpleJSON;
 
 namespace HuntingSuccubus
 {
     public class AutomaticBodySmoother : MVRScript
     {
-        protected JSONStorableFloat Tess;
-        protected JSONStorableFloat TessPhong;
-        protected JSONStorableFloat ScanTiming;
-        protected int timer = 1;
+        private JSONStorableFloat _tess;
+        private JSONStorableFloat _tessPhong;
+        private JSONStorableFloat _scanTiming;
+        private int _timer = 1;
 
         public override void Init()
         {
             try
             {
-                Tess = new JSONStorableFloat("Tess", 3.35f, 1.0f, 8.0f, true, true);
-                RegisterFloat(Tess);
-                CreateSlider(Tess, false);
-                TessPhong = new JSONStorableFloat("TessPhong", 0.5f, 0.0f, 1.0f, true, true);
-                RegisterFloat(TessPhong);
-                CreateSlider(TessPhong, false);
-                ScanTiming = new JSONStorableFloat("Scan Timing", 100f, 10f, 1000f, true, true);
-                RegisterFloat(ScanTiming);
-                CreateSlider(ScanTiming, false);
-
-
+                _tess = new JSONStorableFloat("Tess", 3.35f, 1.0f, 8.0f, true, true);
+                RegisterFloat(_tess);
+                CreateSlider(_tess, false);
+                _tessPhong = new JSONStorableFloat("TessPhong", 0.5f, 0.0f, 1.0f, true, true);
+                RegisterFloat(_tessPhong);
+                CreateSlider(_tessPhong, false);
+                _scanTiming = new JSONStorableFloat("Scan Timing", 100f, 10f, 1000f, true, true);
+                RegisterFloat(_scanTiming);
+                CreateSlider(_scanTiming, false);
             }
             catch (Exception e)
             {
@@ -37,39 +30,35 @@ namespace HuntingSuccubus
             }
         }
 
-        void Update()
+        public void Update()
         {
             try
             {
-                if (timer >= ScanTiming.val)
+                if (_timer >= _scanTiming.val)
                 {
                     foreach (Atom atom in FindObjectsOfType(typeof(Atom)))
                     {
-                        if (atom.type == "Person")
+                        if (atom.type != "Person") continue;
+                        var selector = atom.GetComponentInChildren<DAZCharacterSelector>();
+                        var character = selector.selectedCharacter;
+                        var skin = character.skin;
+                        if (skin == null) return;
+                        foreach (var mat in skin.GPUmaterials)
                         {
-                            var person = atom;
-                            var selector = person.GetComponentInChildren<DAZCharacterSelector>();
-                            var character = selector.selectedCharacter;
-                            var skin = character.skin;
-                            if (skin == null) return;
-                            Material[] mats = skin.GPUmaterials;
-                            foreach (Material mat in mats)
+                            if (mat != null && mat.name != "EyeReflection-1" && mat.name != "EyeReflection" && mat.name != "Irises" && mat.name != "Irises-1" && mat.name != "Eyelashes" && mat.name != "Eyelashes-1" && mat.name != "Cornea-1" && mat.name != "Cornea" && mat.name != "Sclera-1" && mat.name != "Sclera" && mat.name != "Pupils-1" && mat.name != "Pupils" && mat.name != "Tear" && mat.name != "Tear-1" && mat.name != "Teeth-1" && mat.name != "Teeth" && mat.name != "Tongue" && mat.name != "Tongue-1" && mat.name != "InnerMouth-1" && mat.name != "InnerMouth")
                             {
-                                if (mat != null && mat.name != "EyeReflection-1" && mat.name != "EyeReflection" && mat.name != "Irises" && mat.name != "Irises-1" && mat.name != "Eyelashes" && mat.name != "Eyelashes-1" && mat.name != "Cornea-1" && mat.name != "Cornea" && mat.name != "Sclera-1" && mat.name != "Sclera" && mat.name != "Pupils-1" && mat.name != "Pupils" && mat.name != "Tear" && mat.name != "Tear-1" && mat.name != "Teeth-1" && mat.name != "Teeth" && mat.name != "Tongue" && mat.name != "Tongue-1" && mat.name != "InnerMouth-1" && mat.name != "InnerMouth")
-                                {
-                                    if (mat.shader.name != "Custom/Subsurface/TransparentGlossNMNoCullSeparateAlphaComputeBuff")
-                                        mat.shader = Shader.Find("Custom/Subsurface/GlossNMTessMappedFixedComputeBuff");
-                                    mat.SetTexture("_TessTex", Texture2D.whiteTexture);
-                                    mat.SetFloat("_TessPhong", TessPhong.val);
-                                    mat.SetFloat("_Tess", Tess.val);
-                                }
+                                if (mat.shader.name != "Custom/Subsurface/TransparentGlossNMNoCullSeparateAlphaComputeBuff")
+                                    mat.shader = Shader.Find("Custom/Subsurface/GlossNMTessMappedFixedComputeBuff");
+                                mat.SetTexture("_TessTex", Texture2D.whiteTexture);
+                                mat.SetFloat("_TessPhong", _tessPhong.val);
+                                mat.SetFloat("_Tess", _tess.val);
                             }
-                            skin.BroadcastMessage("OnApplicationFocus", true);
                         }
+                        skin.BroadcastMessage("OnApplicationFocus", true);
                     }
-                    timer = 0;
+                    _timer = 0;
                 }
-                timer = timer + 1;
+                _timer += 1;
             }
             catch (Exception e)
             {
