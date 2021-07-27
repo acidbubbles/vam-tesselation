66004a7e08f38f5cb897cc4dd36c5d5e7adfc27b
Acid Bubbles <acidbubbles.anon@gmail.com>
2021-07-26 23:21:55 -0400

    Heavy rewrite to allow per-material tess, colorize preview, restore

--- AutomaticBodySmoother.cs
+++ AutomaticBodySmoother.cs
@@ -1,28 +1,121 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace HuntingSuccubus
 {
     public class AutomaticBodySmoother : MVRScript
     {
-        private JSONStorableFloat _tess;
-        private JSONStorableFloat _tessPhong;
-        private JSONStorableFloat _scanTiming;
-        private int _timer = 1;
+        // ReSharper disable MemberCanBePrivate.Global
+        public const string TessellatedMaskValue = "Tessellated (Masked)";
+        public const string TessellatedNoMaskValue = "Tessellated (Not Masked)";
+        public const string TessellatedValue = "Tessellated";
+        public const string NotTessellatedValue = "Skip";
+        // ReSharper restore MemberCanBePrivate.Global
+
+        public static AutomaticBodySmoother instance;
+
+        public JSONStorableFloat tessJSON { get; private set; }
+        public JSONStorableFloat tessPhongJSON { get; private set; }
+        public JSONStorableBool colorizeJSON { get; private set; }
+
+        private List<JSONStorableStringChooser> materialPolicies { get; set; }
+
+        private JSONStorableStringChooser CreateMaterialJSON(string materialName, bool masked = false, bool active = false, bool editable = true, string label = null)
+        {
+            List<string> choices;
+            string defaultValue;
+            if (!editable)
+            {
+                choices = new List<string> {NotTessellatedValue};
+                defaultValue = NotTessellatedValue;
+            }
+            else if (masked)
+            {
+                choices = new List<string> {TessellatedMaskValue, TessellatedNoMaskValue, NotTessellatedValue};
+                defaultValue = active ? TessellatedNoMaskValue : NotTessellatedValue;
+            }
+            else
+            {
+                choices = new List<string> {TessellatedValue, NotTessellatedValue};
+                defaultValue = active ? TessellatedValue : NotTessellatedValue;
+            }
+
+            var jss = new JSONStorableStringChooser(
+                materialName,
+                choices,
+                defaultValue,
+                materialName,
+                (string _) => ReapplyAll());
+            if (label != null) jss.label = label;
+            return jss;
+        }
+
+        private readonly List<PersonWatcher> _watchers = new List<PersonWatcher>();
+        private Atom _personAtom;
 
         public override void Init()
         {
             try
             {
-                _tess = new JSONStorableFloat("Tess", 3.35f, 1.0f, 8.0f, true, true);
-                RegisterFloat(_tess);
-                CreateSlider(_tess, false);
-                _tessPhong = new JSONStorableFloat("TessPhong", 0.5f, 0.0f, 1.0f, true, true);
-                RegisterFloat(_tessPhong);
-                CreateSlider(_tessPhong, false);
-                _scanTiming = new JSONStorableFloat("Scan Timing", 100f, 10f, 1000f, true, true);
-                RegisterFloat(_scanTiming);
-                CreateSlider(_scanTiming, false);
+                instance = this;
+
+                materialPolicies = new List<JSONStorableStringChooser>
+                {
+                    CreateMaterialJSON("Face", active: true),
+                    CreateMaterialJSON("Head", active: true, label: "Head (Back)"),
+                    CreateMaterialJSON("Ears", active: true),
+                    CreateMaterialJSON("Nostrils", active: true),
+                    CreateMaterialJSON("Lips", active: true),
+                    CreateMaterialJSON("Neck", active: true),
+                    CreateMaterialJSON("Shoulders", masked: true, active: true),
+                    CreateMaterialJSON("Forearms", active: true),
+                    CreateMaterialJSON("Hands", active: true),
+                    CreateMaterialJSON("Torso", masked: true, active: true),
+                    CreateMaterialJSON("Nipples", active: true),
+                    CreateMaterialJSON("Hips", masked: true, active: true),
+                    CreateMaterialJSON("defaultMat", active: true, label: "Gens"),
+                    CreateMaterialJSON("Legs", masked: true, active: true),
+                    CreateMaterialJSON("Feet", active: true),
+                    CreateMaterialJSON("Hips", masked: true, active: true),
+                    CreateMaterialJSON("Toenails", active: true),
+                    CreateMaterialJSON("Fingernails", active: true),
+                    CreateMaterialJSON("Lacrimals", active: true),
+                    CreateMaterialJSON("Gums", active: true),
+                    CreateMaterialJSON("Teeth"),
+                    CreateMaterialJSON("Tongue"),
+                    CreateMaterialJSON("InnerMouth"),
+                    CreateMaterialJSON("EyeReflection", editable: false),
+                    CreateMaterialJSON("Pupils"),
+                    CreateMaterialJSON("Tear", editable: false),
+                    CreateMaterialJSON("Irises", editable: false),
+                    CreateMaterialJSON("Cornea", editable: false),
+                    CreateMaterialJSON("Sclera"),
+                    CreateMaterialJSON("Eyelashes", editable: false),
+                };
+
+                tessJSON = new JSONStorableFloat("Tess", 3.35f, (float _) => ReapplyAll(), 1.0f, 8.0f);
+                RegisterFloat(tessJSON);
+                CreateSlider(tessJSON, false).label = "Tesselation";
+                tessPhongJSON = new JSONStorableFloat("TessPhong", 0.5f, (float _) => ReapplyAll(), 0.0f, 1.0f);
+                RegisterFloat(tessPhongJSON);
+                CreateSlider(tessPhongJSON, false).label = "Phong Strength";
+                colorizeJSON = new JSONStorableBool("Colorize Tessellated Areas", false, (bool _) => ReapplyAll()) {isStorable = false};
+                CreateToggle(colorizeJSON);
+
+                foreach (var materialJSON in materialPolicies)
+                {
+                    RegisterStringChooser(materialJSON);
+                    if (materialJSON.choices.Count == 1) continue;
+                    CreateScrollablePopup(materialJSON, true);
+                }
+
+                if (containingAtom.type == "Person")
+                    _personAtom = containingAtom;
+
+                OnEnable();
             }
             catch (Exception e)
             {
@@ -30,38 +123,175 @@ namespace HuntingSuccubus
             }
         }
 
-        public void Update()
+        private void ReapplyAll()
         {
-            try
+            foreach (var watcher in _watchers)
             {
-                if (_timer >= _scanTiming.val)
+                watcher.Restore();
+                watcher.Apply();
+            }
+        }
+
+        public void OnEnable()
+        {
+            if (containingAtom == null) return;
+            if (!ReferenceEquals(_personAtom, null))
+            {
+                WatchAtom(_personAtom);
+            }
+            else
+            {
+                SuperController.singleton.onAtomUIDsChangedHandlers += SyncAtoms;
+                SyncAtoms(null);
+            }
+
+            StartCoroutine(PeriodicApplyCo());
+        }
+
+        public void OnDisable()
+        {
+            if (ReferenceEquals(_personAtom, null))
+                SuperController.singleton.onAtomUIDsChangedHandlers -= SyncAtoms;
+
+            foreach (var watcher in _watchers)
+            {
+                try
                 {
-                    foreach (var atom in SuperController.singleton.GetAtoms())
-                    {
-                        if (atom.type != "Person") continue;
-                        var selector = atom.GetComponentInChildren<DAZCharacterSelector>();
-                        foreach (var mat in selector.selectedCharacter.skin.GPUmaterials)
-                        {
-                            if (mat != null && mat.name != "EyeReflection-1" && mat.name != "EyeReflection" && mat.name != "Irises" && mat.name != "Irises-1" && mat.name != "Eyelashes" && mat.name != "Eyelashes-1" && mat.name != "Cornea-1" && mat.name != "Cornea" && mat.name != "Sclera-1" && mat.name != "Sclera" && mat.name != "Pupils-1" && mat.name != "Pupils" && mat.name != "Tear" && mat.name != "Tear-1" && mat.name != "Teeth-1" && mat.name != "Teeth" && mat.name != "Tongue" && mat.name != "Tongue-1" && mat.name != "InnerMouth-1" && mat.name != "InnerMouth")
-                            {
-                                if (mat.shader.name != "Custom/Subsurface/TransparentGlossNMNoCullSeparateAlphaComputeBuff")
-                                    mat.shader = Shader.Find("Custom/Subsurface/GlossNMTessMappedFixedComputeBuff");
-                                mat.SetTexture("_TessTex", Texture2D.whiteTexture);
-                                mat.SetFloat("_TessPhong", _tessPhong.val);
-                                mat.SetFloat("_Tess", _tess.val);
-                            }
-                        }
-                        selector.selectedCharacter.skin.BroadcastMessage("OnApplicationFocus", true);
-                    }
-                    _timer = 0;
+                    watcher.Restore();
                 }
-                _timer += 1;
+                catch (Exception exc)
+                {
+                    SuperController.LogError($"AutomaticBodySmoother: Failed to restore watcher. {exc}");
+                }
+
+                Destroy(watcher);
             }
-            catch (Exception e)
+
+            _watchers.Clear();
+        }
+
+        private IEnumerator PeriodicApplyCo()
+        {
+            while (enabled)
             {
-                SuperController.LogError("AutomaticBodySmoother failed to update: " + e);
-                enabledJSON.val = false;
+                yield return new WaitForSeconds(1f);
+
+                for (var i = 0; i < _watchers.Count; i++)
+                {
+                    _watchers[i].Apply();
+                }
             }
+        }
+
+        private void SyncAtoms(List<string> _)
+        {
+            foreach (var person in SuperController.singleton.GetAtoms().Where(a => a.type == "Person"))
+            {
+                WatchAtom(person);
+            }
+        }
+
+        private void WatchAtom(Atom person)
+        {
+            if (person.gameObject.GetComponent<PersonWatcher>() != null) return;
+            var watcher = person.gameObject.AddComponent<PersonWatcher>();
+            _watchers.Add(watcher);
+            watcher.Apply();
+        }
+
+        public bool TryGetPolicy(string materialName, out string policy)
+        {
+            for (var i = 0; i < materialPolicies.Count; i++)
+            {
+                var jss = materialPolicies[i];
+                if (!materialName.StartsWith(jss.name)) continue;
+                policy = jss.val;
+                return true;
+            }
+
+            policy = null;
+            return false;
+        }
+    }
+
+    public class PersonWatcher : MonoBehaviour
+    {
+        private DAZCharacterSelector _selector;
+        private DAZSkinV2 _currentSkin;
+        private readonly List<MaterialSnapshot> _materialSnapshots = new List<MaterialSnapshot>();
+        private static readonly Shader _tessShader = Shader.Find("Custom/Subsurface/GlossNMTessMappedFixedComputeBuff");
+
+        private void Awake()
+        {
+            _selector = gameObject.GetComponentInChildren<DAZCharacterSelector>(true);
+        }
+
+        public void Apply()
+        {
+            if (!ReferenceEquals(_currentSkin, null) && _selector.selectedCharacter.skin == _currentSkin) return;
+            Restore();
+            _currentSkin = _selector.selectedCharacter.skin;
+            if (_currentSkin == null) return;
+            foreach (var mat in _currentSkin.GPUmaterials)
+            {
+                string policy;
+                if (!AutomaticBodySmoother.instance.TryGetPolicy(mat.name, out policy))
+                {
+                    SuperController.LogError($"Could not find any policy for material {mat.name}");
+                    continue;
+                }
+                if (policy == AutomaticBodySmoother.NotTessellatedValue) continue;
+                var snapshot = new MaterialSnapshot
+                {
+                    material = mat,
+                    shader = mat.shader,
+                    tess = mat.GetFloat("_Tess"),
+                    tessPhong = mat.GetFloat("_TessPhong"),
+                    color = mat.GetColor("_Color"),
+                    colorized = AutomaticBodySmoother.instance.colorizeJSON.val,
+                    tessTex = mat.GetTexture("_TessTex")
+                };
+                _materialSnapshots.Add(snapshot);
+                mat.shader = _tessShader;
+                if (policy == AutomaticBodySmoother.TessellatedNoMaskValue)
+                    mat.SetTexture("_TessTex", Texture2D.whiteTexture);
+                mat.SetFloat("_TessPhong", AutomaticBodySmoother.instance.tessPhongJSON.val);
+                mat.SetFloat("_Tess", AutomaticBodySmoother.instance.tessJSON.val);
+                if (AutomaticBodySmoother.instance.colorizeJSON.val)
+                    mat.SetColor("_Color", Color.red);
+            }
+            _selector.selectedCharacter.skin.BroadcastMessage("OnApplicationFocus", true);
+        }
+
+        private void OnDisable()
+        {
+            Restore();
+        }
+
+        public void Restore()
+        {
+            foreach (var materialSnapshot in _materialSnapshots.Where(materialSnapshot => materialSnapshot.material != null))
+            {
+                materialSnapshot.material.shader = materialSnapshot.shader;
+                materialSnapshot.material.SetTexture("_TessTex", materialSnapshot.tessTex);
+                materialSnapshot.material.SetFloat("_TessPhong", materialSnapshot.tessPhong);
+                materialSnapshot.material.SetFloat("_Tess", materialSnapshot.tess);
+                if (materialSnapshot.colorized) materialSnapshot.material.SetColor("_Color", materialSnapshot.color);
+            }
+
+            _materialSnapshots.Clear();
+            _currentSkin = null;
+        }
+
+        private class MaterialSnapshot
+        {
+            public Material material;
+            public Shader shader;
+            public float tessPhong;
+            public float tess;
+            public Texture tessTex;
+            public Color color;
+            public bool colorized;
         }
     }
 }
